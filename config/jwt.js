7f2acef23f759722967706257029fcc5928b99f7
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-09-04 06:45:23 -0500

    feat: :closed_lock_with_key: Generate JWT whit jose

---
+++ config/jwt.js
@@ -0,0 +1,64 @@
+const { MongoLib } = require("../libs/mongodb");
+const dotenv = require("dotenv");
+const {SignJWT, jwtVerify} = require("jose");
+dotenv.config();
+
+class EmployeesCollection {
+    constructor(){
+        this.collection = "Employees",
+        this.mongoDB = new MongoLib()
+    };
+
+    async find(id, pw){
+        try {
+            const employee = await this.mongoDB.getUser(this.collection, id, pw);
+            if (employee === null){
+                throw {status: 404, message: "User not found"}
+            }else{
+                return employee;
+            }
+        } catch (error){
+            throw error
+        }
+    }
+    async generateToken (req,res) {
+        if(Object.keys(req.body).length === 0) throw {status:400,message:"Datos no ingresados"}
+        else{
+            const {id, pw} = req.body
+            const result = await this.find(id, pw);
+            const encoder = new TextEncoder();
+            let data = {
+                id: result.Employee_ID.toString(),
+                role: result.Name
+            }
+        
+            const jwtConstructor = await new SignJWT(data)
+                .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+                .setIssuedAt()
+                .setExpirationTime('3h')
+                .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+        
+            req.data = {status: 200, message: jwtConstructor};
+            return await req.data
+        }
+    }
+    async verifyToken (req, res) {
+        const { authorization } = req.headers;
+        if (!authorization)
+          throw {status: 400, message: "Porfavor generar Token"}
+          try {
+            const encoder = new TextEncoder();
+            req.data = await jwtVerify(
+              authorization,
+              encoder.encode(process.env.JWT_PRIVATE_KEY)
+            );
+            return req.data;
+          } catch (error) {
+            throw {status:498, message: "Algo salio mal, generar un nuevo token"}
+          } 
+      }
+}
+
+
+
+module.exports = {EmployeesCollection};
--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,7 @@ const enclosures = require('./enclosures_routers');
 const purchases = require('./purchases_routers');
 const species = require('./species_routers');
 const visitors = require('./visitors_routers');
+const {appLogin} = require("./login_routes.js")
 const express = require('express');
 
 
@@ -11,6 +12,7 @@ function routerApi(app){
     const router = express.Router();
     app.use('/api/v1', router);
 
+    router.use("/login", appLogin);
     router.use('/animals',animals);
     router.use('/employees',employees);
     router.use('/enclosures',enclosures);
